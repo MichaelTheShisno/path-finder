2fb8f644147109565a72f92bd62df306bbecf6cb
Michael Munoz <mmunoz11@cs.utexas.edu>
2019-05-16 17:07:00 -0500

    Add getNeighbors method to Grid class.

--- src/core/Grid.java
+++ src/core/Grid.java
@@ -1,10 +1,15 @@
 package core;
 
+import java.util.Set;
+import java.util.HashSet;
+
 /**
  * Class that encapsulates the layout of the nodes it holds.
  */
 public class Grid {
     private Node[][] grid;
+    private final static int[][] directions = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
+    private final static int[][] diagonals = {{1, -1}, {1, 1}, {-1, 1}, {-1, -1}};
 
     public Grid(final int ROW_COUNT, final int COL_COUNT) {
         grid = new Node[ROW_COUNT][COL_COUNT];
@@ -33,5 +38,35 @@ public class Grid {
 
     private void setWalkableAt(int row, int col, boolean walkable) {
         this.grid[row][col].setWalkable(walkable);
+    }
+
+    /**
+     * Get a set of all walkable immediate neighbors in the orthogonal direction.
+     * If diagonal nodes are desired, include those too.
+     * @param node The node whose neighbors we are getting.
+     * @param isDiagonal Determines whether diagonal neighbors are allowed in the search.
+     * @return
+     */
+    private Set<Node> getNeighbors(Node node, boolean isDiagonal) {
+        Set<Node> neighbors = new HashSet<>();
+        int row = node.getRow(), col = node.getCol();
+        int adjRow, adjCol;
+        for (int[] direction : directions) {
+            adjRow = row + direction[0];
+            adjCol = col + direction[1];
+            if (this.isWalkable(adjRow, adjCol)) {
+                neighbors.add(grid[adjRow][adjCol]);
+            }
+        }
+        if (isDiagonal) {
+            for (int[] diagonal : diagonals) {
+                adjRow = row + diagonal[0];
+                adjCol = row + diagonal[1];
+                if (this.isWalkable(adjRow, adjCol)) {
+                    neighbors.add(grid[adjRow][adjCol]);
+                }
+            }
+        }
+        return neighbors;
     }
 }
