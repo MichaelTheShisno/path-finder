c78ea21d61201ad6c420fd7759a6a3fec62ef397
Michael Munoz <mmunoz11@cs.utexas.edu>
2019-05-16 14:03:39 -0500

    Add Node class to core

---
+++ src/core/Node.java
@@ -0,0 +1,36 @@
+package core;
+
+public class Node {
+    public final int row, col;
+    public Node parent;
+    public double gScore;
+    public double hScore;
+    public double fScore;
+    public final boolean walkable;
+
+    public Node (final int row, final int col, final boolean walkable) {
+        this.row = row;
+        this.col = col;
+        this.walkable = walkable;
+    }
+
+    public Node() {
+        this(0, 0, false);
+    }
+
+    public int compareTo(Node other) {
+        return Double.compare(this.fScore, other.fScore);
+    }
+
+    public boolean equals(Object obj) {
+        if (obj instanceof Node) {
+            Node other = (Node)obj;
+            return this.row == other.row && this.col == other.col;
+        }
+        return false;
+    }
+
+    public String toString() {
+        return String.format("Node: Row %-3d Col %-3d", this.row, this.col);
+    }
+}
