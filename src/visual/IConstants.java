eae75cda8a5b4f834302969c6e052ba48358a7cb
Michael Munoz <mmunoz11@cs.utexas.edu>
2019-05-17 14:04:09 -0500

    Add color constants to IConstants Interface.

--- src/visual/IConstants.java
+++ src/visual/IConstants.java
@@ -8,4 +8,14 @@ public interface IConstants {
     int NUM_ROWS = 45;
     int NUM_COLS = 80;
     int TILE_SIZE = SCREEN_HEIGHT/TileGrid.NUM_ROWS;
+
+    // Colors used to fill tiles on the grid.
+    Color normalColor = Color.WHITE;
+    Color blockedColor = Color.GRAY;
+    Color startColor = new Color(0, 221, 0);
+    Color endColor = new Color(238, 68, 0);
+    Color openColor = new Color(152, 251, 152);
+    Color closedColor = new Color(175, 238, 238);
+    Color failedColor = new Color(255, 136, 136);
+    Color testedColor = new Color(229, 229, 229);
 }
