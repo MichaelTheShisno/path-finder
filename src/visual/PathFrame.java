6374ce40e9a9e82025bcb5ed4fc1bbe714871aa1
Michael Munoz <mmunoz11@cs.utexas.edu>
2019-05-17 19:31:07 -0500

    Add escape keys to PathFrame class.

--- src/visual/PathFrame.java
+++ src/visual/PathFrame.java
@@ -1,8 +1,10 @@
 package visual;
 
 import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class PathFrame extends JFrame implements IConstants {
+public class PathFrame extends JFrame implements IConstants, KeyListener {
     public TileGrid grid;
     private PathFrameManager vm;
     public PathFrame() {
@@ -14,7 +16,34 @@ public class PathFrame extends JFrame implements IConstants {
         vm = new PathFrameManager(this);
         this.add(grid);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        //this.setUndecorated(true);
+        this.setUndecorated(true);
         this.setVisible(true);
+        addKeyListener(this);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if (exitProgramKeysPressed(e)) {
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    /**
+     * Check if a key event is an exit event.
+     * @param e KeyEvent
+     * @return Returns if any of the escape program keys are hit.
+     */
+    private boolean exitProgramKeysPressed(KeyEvent e) {
+        return e.getKeyChar() == KeyEvent.VK_ESCAPE || e.getKeyChar() == KeyEvent.VK_Q;
     }
 }
